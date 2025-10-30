3aff3f7677b4a4d888daad1df45a0c00e6196e96
Hwirin Kim <kimhuirin@gmail.com>
2025-10-30 19:48:46 +0900

    5장 타입활용하기 정리 완료

---
+++ chapter5/test.ts
@@ -0,0 +1,22 @@
+interface Bank {
+  financialCode: string;
+  companyName: string;
+  name: string;
+  fullName: string;
+}
+
+interface Card {
+  financialCode: string;
+  companyName: string;
+  name: string;
+  appCardType?: string;
+}
+
+type PayMethod<T> = T extends "card" ? Card : Bank;
+type CardPayMethodType = PayMethod<"card">;
+type BankPayMethodType = PayMethod<"bank">;
+
+type PickOne<T> = {
+  [P in keyof T]: Record<P, T[P]> &
+    Partial<Record<Exclude<keyof T, P>, undefined>>;
+}[keyof T];
